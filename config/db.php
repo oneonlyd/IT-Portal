9d4310db5ecb30a1a7088e27392be8028f559c7b
oneonlyd <devinasm05@gmail.com>
2026-06-17 11:08:51 +0900

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,10 +3,10 @@
 // File ini digunakan untuk membuat koneksi tunggal ke database MySQL menggunakan PDO.
 
 // 1. Parameter Konfigurasi Database
-$host     = '127.0.0.1';    // Menggunakan IP local loopback (lebih cepat dibanding 'localhost' di beberapa OS)
+$host     = 'mysql';    // Menggunakan IP local loopback (lebih cepat dibanding 'localhost' di beberapa OS)
 $db       = 'it_portal';    // Nama database yang telah kita buat
 $user     = 'root';         // User utama MySQL
-$pass     = 'Apel1289'; // PENTING: Ganti dengan password baru yang Anda atur di phpMyAdmin sebelumnya!
+$pass     = 'devina123'; // PENTING: Ganti dengan password baru yang Anda atur di phpMyAdmin sebelumnya!
 $charset  = 'utf8mb4';      // Charset yang mendukung penyimpanan teks Unicode modern
 
 // 2. Data Source Name (DSN)
